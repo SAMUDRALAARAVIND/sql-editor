582c4772672ca64e3540bbbd5fd375c41cecbf80
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-07-25 14:06:50 +0530

    Added database credentials

--- apis/connection.js
+++ apis/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql")
 
 const connection = mysql.createConnection({
-    host:"*********************",
-    user:"************",
-    password:"**************",
-    //database:"*************"
+    host:"b0pvcyaxjphwkhrcfnkl-mysql.services.clever-cloud.com",
+    user:"uelrcdmrmg8d8gnz",
+    password:"SWoOWs0wmebIt2e7bg4n",
+    database:"b0pvcyaxjphwkhrcfnkl"
 })
 
 module.exports = connection
